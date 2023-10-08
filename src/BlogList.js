4a295f3bbca8abb62342f3f0731d0a7f45642ae6
centipedejam <jheimburge@gmail.com>
2023-10-08 13:49:27 -0700

    learn useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({blogs, title}) => {
+const BlogList = ({blogs, title, handleDelete}) => {
 
     return ( 
         <div className="blog-list">
@@ -8,6 +8,7 @@ const BlogList = ({blogs, title}) => {
                 <h2>{ blog.title }</h2>
                 <p className="blog-body">{ blog.body }</p>
                 <p>Written by: { blog.author }</p>
+                <button onClick={() => {handleDelete(blog.id)}} >Delete Blog</button>
             </div>
         ))}  
         </div>  
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -6,11 +6,24 @@ const Home = () => {
         {title: 'Blog 1', body: 'lorem...', author: 'Jack', id: 1},
         {title: 'Blog 2', body: 'lorem...', author: 'Zoe', id: 2},
         {title: 'Blog 3', body: 'lorem...', author: 'Yuandan', id: 3},
-        
     ])
+
+const [name, setName] = useState('Jack');
+
+const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+}
+
+useEffect(() => {
+    console.log('name changed');
+}, [name])
+
     return ( 
         <div className="home">
-            <BlogList blogs={ blogs } title="Welcome!"/>
+            <BlogList blogs={ blogs } title="All Blogs" handleDelete={handleDelete}/>
+            <button onClick={() => setName('Zoe')}>Change Name</button>
+            <p>{ name }</p>
         </div>
     );
 }
