6c6d47053e9ae4bb3543f5703e9079d7fd84ea77
centipedejam <jheimburge@gmail.com>
2023-10-08 10:03:54 -0700

    learn useState

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,19 @@
-const Home = () => {
+import { useState } from "react";
 
-    const handleClick = (name, e) => {
-        console.log(name, e.target)
+const Home = () => {
+    let [num, setNum] = useState(1);
+    
+
+    const handleClick = () => {
+        num++
+        setNum(num);
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <button onClick={(e) => {handleClick('Jack', e)}}>Click Me</button>
+            <p>{ num }</p>
+            <button onClick={handleClick}>Click Me</button>
         </div>
      );
 }
